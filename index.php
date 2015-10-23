8301f6d1c2292043a0ee1a97841a40f154c1039b
RuslanKoval <ruslan.koval21@gmail.com>
2015-10-23 19:21:12 +0300

    final

--- index.php
+++ index.php
@@ -10,15 +10,17 @@ use toysBoys\Radio;
 $cars = new Cars([
     'name' => 'BMW',
     'age' => '10',
-    'materials' => 'plastic'
+    'materials' => 'plastic',
+    'price' =>'100',
+    'color' =>'red'
 ]);
 //$cars ->setName('BMW');
 //$cars ->setAge('10 let');
 //$cars ->setMaterials('Plastic');
 //print_r($cars);
 
-echo 'Cars:';
-echo $cars->__toString();
+//echo 'Cars:';
+//echo $cars->__toString();
 $radioCars = new Radio();
 $radioCars->setName('Audi');
 $radioCars->setAge('15 let');
@@ -27,11 +29,35 @@ $radioCars->setBattery('6 chasov');
 $radioCars->setDistance('100 m');
 $radioCars->setMaxSpeed('15 km/h');
 
-echo 'Radio Cars:';
-//echo $radioCars->__toString();
+echo 'Radio Cars: <ul>';
+echo $radioCars->__toString();
+echo '</ul>';
+echo 'Заполнения конструктором<ul>';
 //print_r($cars);
 foreach ($cars->params as $key => $value) {
     echo '<li>';
     print_r($value);
     echo '</li>';
 }
+echo '</ul>';
+
+$radiocars = new Radio([
+    'name' => 'BMW',
+    'age' => '10',
+    'materials' => 'plastic',
+    'battery' =>'100',
+    'maxSpeed' =>'25 k/h',
+    'param1' =>'1',
+    'param2' =>'2',
+    'param3' =>'3',
+    'param4' =>'4'
+]);
+
+echo 'Заполнения конструктором c наследованием<ul>';
+//print_r($cars);
+foreach ($radiocars->params as $key => $value) {
+    echo '<li>';
+    echo $key . ' = ' . $value;
+    echo '</li>';
+}
+echo '</ul>';
